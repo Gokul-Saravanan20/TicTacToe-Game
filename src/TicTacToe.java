1a7698d8eecbd8f06663157da0f5300d6341c4e3
Gokul-Saravanan20 <gokulsaravanan758@gmail.com>
2025-05-08 20:04:42 +0530

    Initial Commit

---
+++ src/TicTacToe.java
@@ -0,0 +1,13 @@
+public class TicTacToe {
+    public static void main(String[] args) {
+        char[][] board = new char[3][3];
+
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                board[row][col] = ' ';
+            }
+        }
+
+
+    }
+}
