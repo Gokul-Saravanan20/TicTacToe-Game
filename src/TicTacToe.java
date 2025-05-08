d8364766c9200caa09a0653c210da4eab141a00d
Gokul-Saravanan20 <gokulsaravanan758@gmail.com>
2025-05-08 21:05:28 +0530

    Game Ready to Play

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,13 +1,66 @@
+import java.util.Scanner;
+
 public class TicTacToe {
     public static void main(String[] args) {
         char[][] board = new char[3][3];
-
-        for (int row = 0; row < 3; row++) {
-            for (int col = 0; col < 3; col++) {
+        int row = 0,col = 0;
+        Scanner scanner = new Scanner(System.in);
+        for (row = 0; row < 3; row++) {
+            for (col = 0; col < 3; col++) {
                 board[row][col] = ' ';
             }
         }
+        char player = 'X';
+        boolean isEnd = false;
 
+        while (!isEnd){
+            printBoard(board);
+            System.out.print("Player " + player + " enter : ");
+             row = scanner.nextInt();
+             col = scanner.nextInt();
 
+            if((row >= 0 && row < 3) && (col >= 0 && col < 3) && board[row][col] == ' '){
+                board[row][col] = player;
+                isEnd = isGameOver(board,player);
+                if(isEnd){
+                    System.out.println("Player "+player + " Won the Match");
+                }else{
+                    player = (player == 'X') ? 'O' : 'X';
+                }
+            }else{
+                System.out.println("!Invalid Move : Try Again");
+            }
+        }
+        printBoard(board);
+
+    }
+
+    private static boolean isGameOver(char[][] board, char player) {
+
+        //check for row
+        for (int row = 0; row < 3; row++) {
+            if(board[row][0] == player && board[row][1] == player && board[row][2] == player) return true;
+        }
+
+        //check for col
+        for (int col = 0; col < 3; col++) {
+            if(board[0][col] == player && board[1][col] == player && board[2][col] == player) return true;
+        }
+
+        //check for diagonal
+        if(board[0][0] == player && board[1][1] == player && board[2][2] == player) return true;
+        else if (board[0][2] == player && board[1][1] == player && board[2][0] == player) return true;
+        return false;
+    }
+
+    private static void printBoard(char[][] board) {
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                System.out.print("| "+ board[row][col] + " " );
+            }
+            System.out.print("|");
+            System.out.println();
+        }
+        System.out.println();
     }
 }
